d27f4ff8bdcff35e2503f73df8913524fcd09746
Isac Bandeira <isac.bandeir4@gmail.com>
2021-09-01 18:38:43 -0300

    envio de um SPA simples com ANGULAR JS

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = angular.module("App", ["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/",{
+        templateUrl:"template/home.html"
+    })
+    .when("/pagina1",{
+        templateUrl:"template/p1.html"
+    })
+    .when("/pagina2",{
+        templateUrl:"template/p2.html"
+    })
+    .when("/pagina3",{
+        templateUrl:"template/p3.html"
+    })
+
+    .otherwise({redirectTo: '/'})
+})
